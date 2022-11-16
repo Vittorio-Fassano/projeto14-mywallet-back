a6e39a22ad5608c800cdda17344a93f9e631e3f3
Vittorio <vittfassano@gmail.com>
2022-11-16 17:42:38 -0300

    feat: sign-in

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import {signUp} from "../controllers/authControllers.js";
+import {signUp, signIn} from "../controllers/authController.js";
 
 const authRouter = Router();
 authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn);
 
 export default authRouter;
