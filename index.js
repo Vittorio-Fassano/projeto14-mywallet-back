b118b02a3cfe406f770972a005a890962d859b17
Vittorio <vittfassano@gmail.com>
2022-11-16 14:45:33 -0300

    feat: connection with database

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import chalk from 'chalk';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+
+
 const app = express();
 
 app.use(cors());
@@ -10,8 +12,9 @@ app.use(express.json());
 
 dotenv.config();
 
-const port = process.env.PORT;
 
+
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(chalk.bold.green(`Server running in port: ${port}`))
 });
