7b9b371a8f348d39f0bd1ca605f0b7fed46e738d
Vittorio <vittfassano@gmail.com>
2022-11-20 18:31:42 -0300

    Final version 1.4 with Deploy

--- src/routes/signInRouter.js
+++ src/routes/signInRouter.js
@@ -4,6 +4,6 @@ import {signIn} from "../controllers/signInController.js";
 import {validatingSignIn} from "../middlewares/signInMiddleware.js";
 
 const signInRouter = Router();
-signInRouter.post("/sign-in", validatingSignIn, signIn);
+signInRouter.post("/", validatingSignIn, signIn);
 
 export default signInRouter;
