6d5ba02b4c93eb6c1a8950158d5a5f90702c788e
Vittorio <vittfassano@gmail.com>
2022-11-20 11:00:53 -0300

    Final Version 1.1 deploy

--- src/controllers/signInController.js
+++ src/controllers/signInController.js
@@ -9,7 +9,7 @@ export async function signIn(req, res) { /*every time you login, a new token is 
         const {user} = res.locals; //get validated user data
         
         await sessionsCollection.insertOne({userId: user._id, token});
-        res.status(200).send({name: user.name, email: user.email}); //sends the validated data to be used in the front-end
+        res.status(200).send({name: user.name, email: user.email, token}); //sends the validated data to be used in the front-end
 
     } catch (err) {
             return res.status(500).send(err);
